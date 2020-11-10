dea329ec2b03a4302ed884b329cc8d16075128a6
Go Inoue <go.inoue@nyu.edu>
2020-11-10 23:33:36 +0900

    Debug

--- scripts/preprocess_raw_text.py
+++ scripts/preprocess_raw_text.py
@@ -11,13 +11,17 @@ from normalize_text import _normalize_text
 def main():
     with open(sys.argv[1], mode='r', encoding='utf-8') as f:
         for line in f.readlines():
-            cleaned = _clean_text(line)
-            filtered = _filter_non_arabic_text(cleaned)
-            if filtered is not None:
-                normalized = _normalize_text(filtered)
-                sys.stdout.write(normalized + '\n')
-            else:
-                sys.stderr.write('This line was filtered.\n')
+            if line == '\n':
+                sys.stdout.write('\n')
+            else
+                cleaned = _clean_text(line)
+                filtered = _filter_non_arabic_text(cleaned)
+                if filtered is not None:
+                    normalized = _normalize_text(filtered)
+                    if normalized:
+                        sys.stdout.write(normalized + '\n')
+                else:
+                    sys.stderr.write('This line was filtered.\n')
 
 
 if __name__ == '__main__':
